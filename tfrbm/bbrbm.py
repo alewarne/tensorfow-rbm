Fixed error in gradient update - now works for dense data.

tfrbm/bbrbm.py
@@ -8,21 +8,20 @@ class BBRBM(RBM):
         RBM.__init__(self, *args, **kwargs)
 
     def _initialize_vars(self):
-        hidden_p = tf.nn.sigmoid(tf.sparse.sparse_dense_matmul(self.x, self.w) + self.hidden_bias)
+        hidden_p = tf.nn.sigmoid(tf.matmul(self.x, self.w) + self.hidden_bias)
         visible_recon_p = sample_bernoulli(tf.nn.sigmoid(tf.matmul(self.y, tf.transpose(self.w)) + self.visible_bias))
         hidden_recon_p = tf.nn.sigmoid(tf.matmul(visible_recon_p, self.w) + self.hidden_bias)
 
-        positive_grad = tf.sparse.sparse_dense_matmul(tf.sparse.transpose(self.x), hidden_p)
+        positive_grad = tf.matmul(tf.transpose(self.x), hidden_p)
         negative_grad = tf.matmul(tf.transpose(visible_recon_p), hidden_recon_p)
 
-        def f(x_old, x_new):
-            # return self.momentum * x_old +\
-            #        self.learning_rate * x_new * (1 - self.momentum) / tf.to_float(tf.shape(x_new)[0])
-            return x_old + self.learning_rate/tf.to_float(x_old.shape[0]) * x_new
+        def momentum(x_old, x_new):
+            return self.momentum * x_old +\
+                   self.learning_rate * x_new * (1 - self.momentum) / tf.to_float(self.batch_size)
 
-        delta_w_new = f(self.delta_w, positive_grad - negative_grad)
-        delta_visible_bias_new = f(self.delta_visible_bias, tf.reduce_sum(tf.sparse.add(self.x, - visible_recon_p), 0))
-        delta_hidden_bias_new = f(self.delta_hidden_bias, tf.reduce_sum(hidden_p - hidden_recon_p, 0))
+        delta_w_new = momentum(self.delta_w, positive_grad - negative_grad)
+        delta_visible_bias_new = momentum(self.delta_visible_bias, tf.reduce_sum(self.x - visible_recon_p, 0))
+        delta_hidden_bias_new = momentum(self.delta_hidden_bias, tf.reduce_sum(hidden_p - hidden_recon_p, 0))
 
         update_delta_w = self.delta_w.assign(delta_w_new)
         update_delta_visible_bias = self.delta_visible_bias.assign(delta_visible_bias_new)
@@ -36,6 +35,6 @@ class BBRBM(RBM):
         self.update_weights = [update_w, update_visible_bias, update_hidden_bias]
 
         self.hidden_recon_p = sample_bernoulli(hidden_recon_p)
-        self.compute_hidden = tf.nn.sigmoid(tf.sparse.sparse_dense_matmul(self.x, self.w) + self.hidden_bias)
+        self.compute_hidden = tf.nn.sigmoid(tf.matmul(self.x, self.w) + self.hidden_bias)
         self.compute_visible = tf.nn.sigmoid(tf.matmul(self.compute_hidden, tf.transpose(self.w)) + self.visible_bias)
         self.compute_visible_from_hidden = tf.nn.sigmoid(tf.matmul(self.y, tf.transpose(self.w)) + self.visible_bias)

tfrbm/rbm.py
@@ -36,9 +36,10 @@ class RBM:
         self.learning_rate = learning_rate
         self.momentum = momentum
 
-        # self.x = tf.placeholder(tf.float32, [None, self.n_visible])
-        self.x = tf.sparse.placeholder(tf.float32, [None, self.n_visible])
+        self.x = tf.placeholder(tf.float32, [None, self.n_visible])
+        # self.x = tf.sparse.placeholder(tf.float32, [None, self.n_visible])
         self.y = tf.placeholder(tf.float32, [None, self.n_hidden])
+        self.batch_size = tf.placeholder(tf.float32)
 
         self.w = tf.Variable(tf_xavier_init(self.n_visible, self.n_hidden, const=xavier_const), dtype=tf.float32)
         self.visible_bias = tf.Variable(tf.zeros([self.n_visible]), dtype=tf.float32)
@@ -70,7 +71,7 @@ class RBM:
             cos_val = tf.reduce_mean(tf.reduce_sum(tf.mul(x1_norm, x2_norm), 1))
             self.compute_err = tf.acos(cos_val) / tf.constant(np.pi)
         else:
-            self.compute_err = tf.reduce_mean(tf.square(tf.sparse.add(self.x, - self.compute_visible)))
+            self.compute_err = tf.reduce_mean(tf.square(self.x - self.compute_visible))
 
         init = tf.global_variables_initializer()
         self.sess = tf.Session()
@@ -80,17 +81,19 @@ class RBM:
         pass
 
     def get_err(self, batch_x):
-        coo = batch_x.tocoo()
-        indices = np.mat([coo.row, coo.col]).transpose()
-        return self.sess.run(self.compute_err, feed_dict={self.x: (indices, coo.data, coo.shape)})
+        # coo = batch_x.tocoo()
+        # indices = np.mat([coo.row, coo.col]).transpose()
+        # return self.sess.run(self.compute_err, feed_dict={self.x: (indices, coo.data, coo.shape)})
+        return self.sess.run(self.compute_err, feed_dict={self.x: batch_x})
 
     def get_free_energy(self):
         pass
 
     def transform(self, batch_x):
-        coo = batch_x.tocoo()
-        indices = np.mat([coo.row, coo.col]).transpose()
-        return self.sess.run(self.compute_hidden, feed_dict={self.x: (indices, coo.data, coo.shape)})
+        # coo = batch_x.tocoo()
+        # indices = np.mat([coo.row, coo.col]).transpose()
+        # return self.sess.run(self.compute_hidden, feed_dict={self.x: (indices, coo.data, coo.shape)})
+        return self.sess.run(self.compute_hidden, feed_dict={self.x: batch_x})
 
     def transform_inv(self, batch_y):
         return self.sess.run(self.compute_visible_from_hidden, feed_dict={self.y: batch_y})
@@ -99,10 +102,12 @@ class RBM:
         return self.sess.run(self.compute_visible, feed_dict={self.x: batch_x})
 
     def partial_fit(self, batch_x, batch_x_tilde):
-        coo = batch_x.tocoo()
-        indices = np.mat([coo.row, coo.col]).transpose()
-        ret = self.sess.run([self.update_weights + self.update_deltas + [self.hidden_recon_p]], feed_dict={self.x: (indices, coo.data, coo.shape),
-                                                                             self.y: batch_x_tilde})
+        # coo = batch_x.tocoo()
+        # indices = np.mat([coo.row, coo.col]).transpose()
+        # ret = self.sess.run([self.update_weights + self.update_deltas + [self.hidden_recon_p]], feed_dict={self.x: (indices, coo.data, coo.shape),
+        #                                                                      self.y: batch_x_tilde})
+        ret = self.sess.run([self.update_weights + self.update_deltas + [self.hidden_recon_p]],
+                            feed_dict={self.x: batch_x, self.y: batch_x_tilde, self.batch_size: batch_x.shape[0]})
         # return the hidden reconstruction for the persitent contrastive divergence algorithm
         return ret[0][-1]
 
@@ -130,7 +135,6 @@ class RBM:
         assert n_epoches > 0
 
         n_data = data_x.shape[0]
-
         if batch_size > 0:
             n_batches = n_data // batch_size + (0 if n_data % batch_size == 0 else 1)
         else:
@@ -144,8 +148,8 @@ class RBM:
 
         errs = []
 
-        # x_tilde = np.zeros((batch_size, self.n_hidden))
-        x_tilde = self.transform(data_x[:batch_size])
+        x_tilde = np.zeros((batch_size, self.n_hidden))
+        # x_tilde = self.transform(data_x[:batch_size])
         for e in range(n_epoches):
             if verbose and not self._use_tqdm:
                 print('Epoch: {:d}'.format(e))

tfrbm/util.py
@@ -6,8 +6,9 @@ import tensorflow as tf
 
 
 def tf_xavier_init(fan_in, fan_out, *, const=1.0, dtype=np.float32):
-    k = const * np.sqrt(6.0 / (fan_in + fan_out))
-    return tf.random_uniform((fan_in, fan_out), minval=-k, maxval=k, dtype=dtype)
+    # k = const * np.sqrt(6.0 / (fan_in + fan_out))
+    # return tf.random_uniform((fan_in, fan_out), minval=-k, maxval=k, dtype=dtype)
+    return tf.random_normal((fan_in, fan_out), 0.0, 0.01)
 
 
 def sample_bernoulli(probs):

training.py
@@ -4,12 +4,16 @@ import numpy as np
 import pickle as pkl
 import argparse
 from sklearn.neural_network import BernoulliRBM
+from tfrbm.util import load_npy_npz_pkl
 
 
 def get_data(data_path, label_path):
     np.random.seed(42)
-    data = sp.load_npz(data_path)
-    labels = np.argmax(np.load(label_path), axis=1)
+    data = load_npy_npz_pkl(data_path)
+    # data = sp.csr_matrix(data)
+    labels = np.load(label_path)
+    if len(labels.shape) > 1:
+        labels = np.argmax(labels, axis=1)
     data_1 = data[np.where(labels == 1)]
     # random_indices = np.random.choice(np.where(labels == 0)[0], data_1.shape[0], replace=False)
     # data = sp.vstack([data_1, data[random_indices]])