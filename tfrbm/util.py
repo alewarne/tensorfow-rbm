3ebffd3fef9bf6d402b81410e668e5373e0e8dab
awarnecke <a.warnecke@sec.tu-bs.de>
2019-07-05 10:07:17 +0200

    General data loading method

--- tfrbm/util.py
+++ tfrbm/util.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pickle as pkl
+import scipy.sparse as sp
+import os
 import tensorflow as tf
 
 
@@ -13,3 +16,18 @@ def sample_bernoulli(probs):
 
 def sample_gaussian(x, sigma):
     return x + tf.random_normal(tf.shape(x), mean=0.0, stddev=sigma, dtype=tf.float32)
+
+
+# takes a filepath and loads the contained data for the data formats .npy, .pkl, .npz
+def load_npy_npz_pkl(path_to_data):
+    _, filetype = os.path.splitext(path_to_data)
+    if filetype == '.npz':
+        data = sp.load_npz(path_to_data)
+    elif filetype == '.npy':
+        data = np.load(path_to_data)
+    elif filetype == '.pkl':
+        data = pkl.load(open(path_to_data, 'rb'))
+    else:
+        print('Could not load filepath! Invalid datatype!')
+        data = None
+    return data
